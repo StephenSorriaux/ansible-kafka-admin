b814e056d39b56b3de1af9572b1f0b53b50fcdf6
Paul Steppacher <paul.steppacher@squer.at>
2022-05-27 12:29:24 +0200

    add improved single acl edge case logic

--- module_utils/kafka_lib_acl.py
+++ module_utils/kafka_lib_acl.py
@@ -33,6 +33,7 @@ def process_module_acl(module):
         'state': params['state']
     }]
     params['mark_others_as_absent'] = False
+    params['single_acl'] = True
     process_module_acls(module, params)
 
 
@@ -62,8 +63,29 @@ def process_module_acls(module, params=None):
                     'instead'
                 )
 
-        acl_resources_found = manager.describe_acls(
-            ACLResource(
+        if params['single_acl']:
+            acl = acls[0]
+            acl_name = acl['name']
+            acl_resource_type = acl['acl_resource_type']
+            acl_principal = acl['acl_principal']
+            acl_operation = acl['acl_operation']
+            acl_permission = acl['acl_permission']
+            acl_pattern_type = acl['acl_pattern_type']
+            acl_host = acl['acl_host']
+
+            acl_resource = ACLResource(
+                resource_type=ACLResourceType.from_name(acl_resource_type),
+                operation=ACLOperation.from_name(acl_operation),
+                permission_type=ACLPermissionType.from_name(
+                    acl_permission
+                ),
+                pattern_type=ACLPatternType.from_name(acl_pattern_type),
+                name=acl_name,
+                principal=acl_principal,
+                host=acl_host
+            )
+        else:
+            acl_resource = ACLResource(
                 resource_type=ACLResourceType.ANY,
                 operation=ACLOperation.ANY,
                 permission_type=ACLPermissionType.ANY,
@@ -71,7 +93,9 @@ def process_module_acls(module, params=None):
                 name=None,
                 principal=None,
                 host=None
-            ), api_version
+            )
+        acl_resources_found = manager.describe_acls(
+            acl_resource, api_version
         )
 
         acls_marked_present = [ACLResource(
